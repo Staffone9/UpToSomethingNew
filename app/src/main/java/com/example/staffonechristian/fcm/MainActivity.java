1eb9d7a048346ca6df407eb38f3dc8aa8dbd000c
Staffone Christian <staffonechristian@Staffones-MacBook-Air.local>
2017-02-27 17:16:20 -0500

    first commit

---
+++ app/src/main/java/com/example/staffonechristian/fcm/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.staffonechristian.fcm;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText titleText;
+    EditText DetailText;
+    Button sendButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+        //because of network exception
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+        titleText = (EditText) findViewById(R.id.TitleId);
+        DetailText = (EditText) findViewById(R.id.DetailId);
+        sendButton = (Button) findViewById(R.id.SendId);
+        System.out.println("MainActivity.onCreate: " + FirebaseInstanceId.getInstance().getToken());
+
+    }
+
+
+    public static String makeRequest(String title,String Detail) throws JSONException {
+        HttpURLConnection urlConnection;
+        JSONObject json = new JSONObject();
+        JSONObject info = new JSONObject();
+        info.put("title", title );   // Notification title
+        info.put("body", Detail); // Notification body
+        info.put("sound", "default"); // Notification sound
+        json.put("notification", info);
+        json.put("to","/topics/news");
+
+        Log.e("jsonn==> ",json.toString());
+        String data = json.toString();
+        String result = "kaipan";
+        try {
+            //Connect
+            urlConnection = (HttpURLConnection) ((new URL("https://fcm.googleapis.com/fcm/send").openConnection()));
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.setRequestProperty("Authorization", "key=AIzaSyAxZioY3tZ6RDHm40Y2gwD6bKZVjVmch0A");
+            urlConnection.setRequestMethod("POST");
+            urlConnection.connect();
+
+            //Write
+            OutputStream outputStream = urlConnection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+            writer.write(data);
+            writer.close();
+            outputStream.close();
+
+            //Read
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+            String line = null;
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+
+            bufferedReader.close();
+            result = sb.toString();
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public void SendNotification(View view) {
+        try {
+            makeRequest(titleText.getText().toString(),DetailText.getText().toString());
+            //  Toast.makeText(this,makeRequest(1+""),Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/staffonechristian/fcm/MyFirebaseInstanceIDService.java
@@ -0,0 +1,25 @@
+package com.example.staffonechristian.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by staffonechristian on 2017-02-25.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static String TAG = "Registration";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        System.out.println("Registration.onTokenRefresh TOKEN: " + refreshedToken );
+    }
+}
